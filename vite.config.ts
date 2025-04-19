00475bcd04193368c73fd046b4870cbe5c61ae9a
Jian Lv <lvjian700@gmail.com>
2025-04-19 23:34:52 +1000

    Update root url

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  // Base public path when served from GitHub Pages under /ihahits/
+  base: '/ihahits/',
   plugins: [react()]
 });
