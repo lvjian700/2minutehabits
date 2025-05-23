a0fd8dd80a028061b919c6851d0b5f169029dcbc
Jian Lv <lvjian700@gmail.com>
2025-05-23 22:31:16 +1000

    Support pwa with new background color

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+// Simple service worker for PWA
+const CACHE_NAME = 'ivotes-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  // Add more assets or routes as needed
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
