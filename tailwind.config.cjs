c1b8f78f5dd5e11bd94e648e2fb1792dee83b424
Jian Lv <lvjian700@gmail.com>
2025-05-24 00:12:03 +1000

    Refactor - introducing title color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'color-title': '#1E3A5F'
+      }
+    }
   },
   plugins: []
 };
